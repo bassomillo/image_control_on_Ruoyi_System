0e09b0e2003df20626e33fa4dd3dfe723dab47fc
zhaom38 <zhaom38@chinaunicom.cn>
2021-05-14 09:39:13 +0800

    网页管理-栏目管理（上传WebArticleCategoryBtreeVO）

---
+++ ruoyi/src/main/java/com/ruoyi/project/monitor/domain/VO/WebArticleCategoryBtreeVO.java
@@ -0,0 +1,33 @@
+package com.ruoyi.project.monitor.domain.VO;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class WebArticleCategoryBtreeVO {
+    private Integer id;
+
+    private String name;
+
+    private Integer weight;
+
+    private Integer publishArticle;
+
+    private String seoTitle;
+
+    private String seoKeyword;
+
+    private String seoDesc;
+
+    private Integer published;
+
+    private Integer parentId;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createdTime;
+
+    private List<WebArticleCategoryVO> children;
+}
