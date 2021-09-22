26bfa947055c4583002bb6c65bc18df4e539c1d2
bassomillo <rcai006@uottawa.ca>
2021-09-21 23:14:04 -0400

    add 2 database

---
+++ ruoyi/sql/images.sql
@@ -0,0 +1,5 @@
+INSERT INTO `images` VALUES (1, 'string', 'string', 'string', 0, 'string', 'string', '2021-9-22 08:11:06', '2021-9-22 08:11:06');
+INSERT INTO `images` VALUES (2, 'string', 'cat', 'animal', 0, 'string', 'cat animal', '2021-9-22 08:22:30', '2021-9-22 08:22:30');
+INSERT INTO `images` VALUES (3, 'string', 'dog', 'animal', 0, 'string', 'dog animal', '2021-9-22 08:22:59', '2021-9-22 08:22:59');
+INSERT INTO `images` VALUES (4, 'string', 'lion', 'animal', 1, 'string', 'lion animal', '2021-9-22 08:23:13', '2021-9-22 08:23:13');
+INSERT INTO `images` VALUES (6, 'string1', 'lions', 'animal', 1, 'string1', 'lions animal', '2021-9-22 08:24:36', '2021-9-22 08:24:36');
